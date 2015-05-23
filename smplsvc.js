aecb8fbdfe2d05faaa4753d240ab427389258462
Arthur Keys <arkeys@gmail.com>
2015-05-22 22:28:21 -0700

    Update smplsvc.js

--- smplsvc.js
+++ smplsvc.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to the memo!\n');
+  res.end('Welcome to the demo!\n');
 }).listen(80);
 console.log('Server running on port 80');
